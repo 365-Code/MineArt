25afc041374669815aa0e197661b5663d0a2af89
365-Code <sohailahmedbhati786@gmail.com>
2024-02-08 17:42:39 +0530

    some ui changes

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
   return (
     <main className='flex container1 py-[100px] items-center justify-center flex-col'>
-    <p className='text-3xl text-rose-500'>Loading...</p>
+    <p className='text-3xl text-rose-500 font-semibold'>Loading...</p>
     <p className='text-xl '>It won&apos;t take much long :)</p>
     </main>
   )
